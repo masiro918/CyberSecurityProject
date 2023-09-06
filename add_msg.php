8121ab1cb587f026450f58d8a56eb086169d436c
Matias Siro <76435365+masiro918@users.noreply.github.com>
2023-09-06 17:12:40 +0300

    Update add_msg.php

--- add_msg.php
+++ add_msg.php
@@ -13,10 +13,6 @@
 
 include("db_conn.php");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $username = $_POST["username"];
 $message = $_POST["message"];
 
