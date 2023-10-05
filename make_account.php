96e5ed2a04d34901f7ed02aa2d684fef62c926c1
Matias Siro <76435365+masiro918@users.noreply.github.com>
2023-10-05 16:42:07 +0300

    Update make_account.php

--- make_account.php
+++ make_account.php
@@ -2,10 +2,6 @@
 
 include("db_conn.php");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $username = $_POST["username"];
 $password = $_POST["password"];
 
